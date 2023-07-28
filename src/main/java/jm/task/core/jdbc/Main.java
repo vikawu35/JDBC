5b3c8948e3b564e00c5aed5432de8de007045c8e
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-07-28 10:52:40 +0900

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,32 +1,22 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        List<User> usersToAdd = new ArrayList<>();
-        usersToAdd.add(new User("John", "Doe", (byte) 30));
-        usersToAdd.add(new User("Jane", "Smith", (byte) 35));
-        usersToAdd.add(new User("Michael", "Johnson", (byte) 45));
-        usersToAdd.add(new User("Emily", "Brown", (byte) 26));
+        UserDaoJDBCImpl user = new UserDaoJDBCImpl();
+        user.createUsersTable();
 
-        for (User user : usersToAdd) {
-            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
-            System.out.println("User with name - " + user.getName() + " added to the database.");
-        }
-        List<User> users = userService.getAllUsers();
-        for (User user : users) {
-            System.out.println(user);
-        }
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        user.saveUser("John", "Doe", (byte) 30);
+        user.saveUser("Jane", "Smith", (byte) 35);
+        user.saveUser("Michael", "Johnson", (byte) 45);
+        user.saveUser("Emily", "Brown", (byte) 26);
 
-
+        user.getAllUsers();
+        System.out.println(user.getAllUsers().toString());
+        user.cleanUsersTable();
+        user.dropUsersTable();
     }
 }
