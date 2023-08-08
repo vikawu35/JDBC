c8c20a67ed5aff05afb55ff642610031d33dea79
vikawu35 <122668662+vikawu35@users.noreply.github.com>
2023-08-08 13:40:54 +0900

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,14 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoJDBCImpl user = new UserDaoJDBCImpl();
+        UserServiceImpl user = new UserServiceImpl();
         user.createUsersTable();
 
         user.saveUser("John", "Doe", (byte) 30);
@@ -14,8 +16,9 @@ public class Main {
         user.saveUser("Michael", "Johnson", (byte) 45);
         user.saveUser("Emily", "Brown", (byte) 26);
 
-        user.getAllUsers();
-        System.out.println(user.getAllUsers().toString());
+        List<User> userList = user.getAllUsers();
+        userList.forEach(System.out::println);
+
         user.cleanUsersTable();
         user.dropUsersTable();
     }
